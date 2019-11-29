46ca2f6679a3b0f6ac92d98257ef2cda5b173679
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2019-11-29 11:50:04 +0100

    hola

---
+++ exemplostring/ExemploString.java
@@ -0,0 +1,15 @@
+
+package exemplostring;
+
+
+public class ExemploString {
+
+   
+    public static void main(String[] args) {
+        
+    // crear un String con cadea1 co valor Daniel Castelao y un String cadea2 co valor impega
+    
+        System.out.println("hola");
+    }
+    
+}
